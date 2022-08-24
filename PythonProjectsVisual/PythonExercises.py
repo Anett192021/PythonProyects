c04f28aac02141872e3f3d24fb40afb430e08f73
Anett192021 <101136626+Anett192021@users.noreply.github.com>
2022-08-23 23:45:17 -0600

    Add files via upload

---
+++ PythonProjectsVisual/PythonExercises.py
@@ -0,0 +1,12 @@
+def main[]:
+    primerNombre = input("Ingrese el primer nombre; "):
+    primerApellido = input("Ingrese el primer apellido; "): 
+
+    result = primerNombre + "," + primerApellido + "gest.uca.edu.ni":
+
+    print("El correo institucional es: " +  result):
+
+    main()
+    
+
+
